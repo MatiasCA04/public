f886fe54263ef50316e0f96602c9809ef6bffe52
Matias Almeida <mathiascalmeida@gmail.com>
2025-10-02 14:26:50 +0100

    Started PHP Laravel Project Currently has: -Login and Register working, controllers, data base and front end -Task Manager working 50%, basic functionalities (CRUD), front end started but not finished

---
+++ TaskMaster/resources/views/auth/login.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Task Manager</title>
+    {{-- Load CSS with Vite --}}
+    @vite('resources/css/login.css')
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <div class="wrapper">
+        <form action="{{ route('login.submit') }}" method="POST">
+            @csrf
+            <h1>Login</h1>
+            <div class="input-box">
+                <input type="text" name="email" placeholder="Email" required>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" name="password" placeholder="Password" required>
+                <i class='bx bxs-lock-alt' ></i>
+            </div>
+            <div class="remember-forgot">
+                <label><input type="checkbox">Remember Me</label>
+                <a href="#">Forgot Password</a>
+            </div>
+            <button type="submit" class="btn">Login</button>
+            <div class="register-link">
+                <p>Don't have an account? <a href="/register">Register</a></p>
+            </div>
+        </form>
+
+        @if($errors->any())
+            <div class="errors">
+                <p>{{ $errors->first() }}</p>
+            </div>
+        @endif
+    </div>
+</body>
+</html>
